1308590ea85ad43f51fa78baa962145c0dee8b08
Chen-Han Hsiao (Stanley) <chenhan.hsiao.tw@gmail.com>
2018-11-23 16:33:08 +0800

    temp dataset

--- kitti/exec_3d.sh
+++ kitti/exec_3d.sh
@@ -15,7 +15,7 @@ wstool init src
 wstool merge -t src https://raw.githubusercontent.com/googlecartographer/cartographer_ros/master/cartographer_ros.rosinstall
 wstool set cartographer --git https://github.com/googlecartographer/cartographer.git -v master -t src -y
 wstool set cartographer_ros --git https://github.com/googlecartographer/cartographer_ros.git -v master -t src -y
-wstool update -t src
+wstool update -t src -v
 
 # Install deb dependencies.
 # The command 'sudo rosdep init' will print an error if you have already
@@ -25,11 +25,14 @@ rosdep update
 rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
 
 # Build and install.
-catkin_make_isolated --install --use-ninja
-source install_isolated/setup.bash
+# catkin_make_isolated --install --use-ninja
+tar xzvf ../cartographer.tar.gz -C / 
+source /opt/cartographer/setup.bash
 
 cd $DIR
 # Download the 3D backpack example bag.
+./gdown.pl https://drive.google.com/file/d/1U_j7WskboqmApCoR176ldd-D7qhUvs3K/view\?usp\=sharing kitti_2011_09_30_drive_0028_synced.bag
+# ./gdown.pl https://drive.google.com/file/d/12xkd8q8SlY427k_oatfICpFKgaJmhvpP/view?usp=sharing kitti_2011_09_26_drive_0117_synced.bag
 
 # run cartographer
-./xvfb.py $DIR/record-cartographer.sh roslaunch $DIR/demo.launch rviz_config:=$DIR/online.rviz bag_filenames:=$DIR/kitti_2011_09_26_drive_0117_synced.bag
+./xvfb.py $DIR/record-cartographer.sh roslaunch $DIR/demo.launch rviz_config:=$DIR/online.rviz bag_filename:=$DIR/kitti_2011_09_30_drive_0028_synced.bag
